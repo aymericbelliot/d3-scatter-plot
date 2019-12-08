1903cce82278b584cc344131b39d738cac8bb677
aymericbelliot <aymericbelliot@MacBook-de-Aymeric.local>
2019-12-08 18:24:23 -0400

    2.0 ok

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      //document.getElementById("data").innerHTML = JSON.stringify(data);
       populateScatterPlot("#scatterPlot", data);
     })
 });
@@ -34,14 +33,14 @@ populateScatterPlot = (id, dataset) => {
 
   // Define scaling functions
   const xScale = d3.scaleLinear()
-    .domain([d3.min(dataset, (d) => setDataYear(d.Year)),
-    d3.max(dataset, (d) => setDataYear(d.Year))])
+    .domain([d3.min(dataset, d => setDataYear(d.Year)),
+    d3.max(dataset, d => setDataYear(d.Year))])
     .range([padding, w - padding]);
 
   const yScale = d3.scaleLinear()
-    .domain([d3.min(dataset, (d) => setDataTime(d.Time)),
-    d3.max(dataset, (d) => setDataTime(d.Time))])
-    .range([h - padding, padding]);
+    .domain([d3.min(dataset, d => setDataTime(d.Time)),
+    d3.max(dataset, d => setDataTime(d.Time))])
+    .range([padding, h - padding]);
 
   // Create SVG layout
   const svg = d3.select(id)
@@ -63,97 +62,54 @@ populateScatterPlot = (id, dataset) => {
     .attr("transform", "translate(" + padding + ", 0)")
     .call(yAxis)
     .attr("id", "y-axis");
-}/*
+
   // Add y axis label
   svg.append("text")
     .attr("x", - h / 2 - padding)
-    .attr("y", padding / 2)
+    .attr("y", padding + 20)
     .attr("transform", "rotate(270)")
     .text("Time in minutes");
 
-  // Create bars 
-  svg.selectAll("circles")
+  // Create plots 
+  svg.selectAll("circle")
     .data(dataset)
     .enter()
-    .append("circles")
-    .attr("cx", (d, i) => xScale(d.year)
-    .attr("cy", (d, i) => yScale(d.time))
-    .attr("r", 10)
+    .append("circle")
+    .attr("cx", d => xScale(setDataYear(d.Year)))
+    .attr("cy", d => yScale(setDataTime(d.Time)))
+    .attr("r", 5)
     .attr("class", "dot")
-    .attr("data_xvalue", (d, i) => d.year)
-    .attr("data_yvalue", (d, i) => d.time)
+    .attr("data_xvalue", d => d.Year)
+    .attr("data_yvalue", d => d.Time)
+    .style("fill", d => d.Doping == "" ? "red" : "blue")
     .append("title")
-    .text(d => d[0].slice(0, 4) + " Q" + ((d[0].slice(5, 7) - 1) / 3 + 1) + "\n$ " + d[1] + " Billion"));
+    .attr("id", "tooltip")
+    .attr("data-year", d => d.Year)
+    .text((d) => d.Name + " : " + d.Nationality + "\nYear : " + d.Year + ", Time : " + d.Time + "\n\n" + d.Doping);
+
+  // Add legend 
+  svg.append("rect")
+    .attr("x", w - padding)
+    .attr("y", h / 2)
+    .attr("width", 20)
+    .attr("height", 20)
+    .style("fill", "blue");
+
+  svg.append("rect")
+    .attr("x", w - padding)
+    .attr("y", h / 2 + 30)
+    .attr("width", 20)
+    .attr("height", 20)
+    .style("fill", "red");
+
+  svg.append("text")
+    .attr("x", w - padding - 137)
+    .attr("y", h / 2 + 16)
+    .text("Doping allegation")
+    .attr("id", "legend");
+
+  svg.append("text")
+    .attr("x", w - padding - 160)
+    .attr("y", h / 2 + 46)
+    .text("No doping allegation");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*populateBarChart = (id, dataset) => {
- // Define w/h/p ratio
- const w = 1000;
- const h = 500;
- const padding = 50;
-
- // Define scaling functions
- const xScale = d3.scaleLinear()
-   .domain([d3.min(dataset, (d, i) => d[0]).slice(0, 4), d3.max(dataset, (d, i) => d[0]).slice(0, 4)])
-   .range([padding, w - padding]);
-
- const yScale = d3.scaleLinear()
-   .domain([0, d3.max(dataset, (d, i) => d[1])])
-   .range([h - padding, padding]);
-
- // Create SVG layout
- const svg = d3.select(id)
-   .append("svg")
-   .attr("viewBox", "0 0 " + w + " " + h)
-
- // Create Axes
- const xAxis = d3.axisBottom(xScale);
- const yAxis = d3.axisLeft(yScale);
-
- svg.append("g")
-   .attr("transform", "translate(0," + (h - padding) + ")")
-   .call(xAxis)
-   .attr("id", "x-axis");
-
- svg.append("g")
-   .attr("transform", "translate(" + padding + ", 0)")
-   .call(yAxis)
-   .attr("id", "y-axis");
-
- // Add y axis label
- svg.append("text")
-   .attr("x", - h / 2 - padding)
-   .attr("y", padding + 30)
-   .attr("transform", "rotate(270)")
-   .text("Gross Domestic product");
-
- // Create bars
- svg.selectAll("rect")
-   .data(dataset)
-   .enter()
-   .append("rect")
-   .attr("x", (d, i) => xScale(d[0].slice(0, 4) + "." + (d[0].slice(5, 7) - 1) * 100 / 12))
-   .attr("y", (d, i) => yScale(d[1]))
-   .attr("width", (d, i) => (w - 2 * padding) / dataset.length)
-   .attr("height", (d, i) => h - padding - yScale(d[1]))
-   .attr("class", "bar")
-   .attr("data_date", (d, i) => d[0])
-   .attr("data_gdp", (d, i) => d[1])
-   .append("title")
-   .text(d => d[0].slice(0, 4) + " Q" + ((d[0].slice(5, 7) - 1) / 3 + 1) + "\n$ " + d[1] + " Billion");
-}*/
